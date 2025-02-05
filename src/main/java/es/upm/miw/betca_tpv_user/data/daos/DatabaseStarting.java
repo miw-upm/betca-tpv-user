8a7349c9a9541d3b1bd428667476193204ff457a
JBernal <j.bernal@upm.es>
2025-02-05 14:17:31 +0100

    Version 4.5.13 & BD on render

--- src/main/java/es/upm/miw/betca_tpv_user/data/daos/DatabaseStarting.java
+++ src/main/java/es/upm/miw/betca_tpv_user/data/daos/DatabaseStarting.java
@@ -4,6 +4,7 @@ import es.upm.miw.betca_tpv_user.data.model.Role;
 import es.upm.miw.betca_tpv_user.data.model.User;
 import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Repository;
 
@@ -17,15 +18,19 @@ public class DatabaseStarting {
     private static final String MOBILE = "6";
     private static final String PASSWORD = "6";
 
+    private final Environment environment;
     private final UserRepository userRepository;
 
     @Autowired
-    public DatabaseStarting(UserRepository userRepository) {
+    public DatabaseStarting(UserRepository userRepository, Environment environment) {
         this.userRepository = userRepository;
+        this.environment = environment;
         this.initialize();
     }
 
     void initialize() {
+        LogManager.getLogger(this.getClass()).info(() -> "--- Data Base: {}" + this.environment.getProperty("spring.datasource.url"));
+        LogManager.getLogger(this.getClass()).info(() -> "--- JWT secret: {}" + this.environment.getProperty("miw.jwt.secret"));
         LogManager.getLogger(this.getClass()).warn("------- Finding Admin -----------");
         if (this.userRepository.findByRoleIn(List.of(Role.ADMIN)).isEmpty()) {
             User user = User.builder().mobile(MOBILE).firstName(SUPER_USER)
